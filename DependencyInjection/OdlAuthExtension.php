9b5898904bef6d096e23a1a6cad114d4c2c49595
David Tee <davidkmo@gmail.com>
2011-05-08 16:47:39 -0700

    Forgot to check in a few files.

---
+++ DependencyInjection/OdlAuthExtension.php
@@ -0,0 +1,26 @@
+<?php
+namespace Odl\AuthBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+class OdlAuthExtension
+	extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    	v('in auth extension');
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
+        // Lets set up
+    }
+
+    public function getAlias()
+    {
+        return 'odl_auth';
+    }
+}
